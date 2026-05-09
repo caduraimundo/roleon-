5484944fa6655c9fa12a6c70fbb1cd6a6f3ab39f
Carlos Raimundo <caduoliveiraraimundo@gmail.com>
2026-05-09 16:38:30 -0300

    fix: auth callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -5,23 +5,14 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-
   if (code) {
     const cookieStore = cookies()
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          getAll() { return cookieStore.getAll() },
-          setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
-          },
-        },
-      }
+      { cookies: { getAll() { return cookieStore.getAll() }, setAll(cookiesToSet) { cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options)) } } }
     )
     await supabase.auth.exchangeCodeForSession(code)
   }
-
   return NextResponse.redirect(origin + '/')
 }
