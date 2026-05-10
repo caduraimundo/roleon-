38577275a5f1d90eeb44aacde6ff0d8e8893604e
Carlos Raimundo <caduoliveiraraimundo@gmail.com>
2026-05-09 23:34:38 -0300

    fix: await cookies no auth callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   if (code) {
-    const cookieStore = cookies()
+    const cookieStore = await cookies()
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
