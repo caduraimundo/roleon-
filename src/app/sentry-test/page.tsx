d9b4b454b1e90901fd9c2bc3fe68e61b3b0c5afd
Carlos Raimundo <caduoliveiraraimundo@gmail.com>
2026-05-20 19:51:10 -0300

    test: melhorar página de teste do Sentry

--- src/app/sentry-test/page.tsx
+++ src/app/sentry-test/page.tsx
@@ -1,7 +1,18 @@
 'use client'
+import * as Sentry from "@sentry/nextjs"
+
 export default function SentryTest() {
+  const handleTest = () => {
+    try {
+      throw new Error('Teste Sentry Roleon - ' + new Date().toISOString())
+    } catch (e) {
+      Sentry.captureException(e)
+      alert('Erro enviado ao Sentry!')
+    }
+  }
+
   return (
-    <button onClick={() => { throw new Error('Teste Sentry Roleon') }}>
+    <button onClick={handleTest}>
       Disparar erro de teste
     </button>
   )
