62ef32bf8bedd3c669ed7079df7c45fa5ed9c7a7
Carlos Raimundo <caduoliveiraraimundo@gmail.com>
2026-05-29 13:27:11 -0300

    fix: migrar para AdvancedMarkerElement com estilo via Cloud

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places,marker&loading=async`}
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places,marker&v=beta&loading=async`}
           strategy="beforeInteractive"
         />
         <ServiceWorkerRegistrar />
