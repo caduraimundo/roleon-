8a55a016a816f19c3e5c33e11d4561a80c11f175
Carlos Raimundo <caduoliveiraraimundo@gmail.com>
2026-05-29 08:51:52 -0300

    fix: subir nav bar e remover labels de rodovias

--- src/components/MapClient.tsx
+++ src/components/MapClient.tsx
@@ -31,6 +31,8 @@ const LIGHT_MAP_STYLE: google.maps.MapTypeStyle[] = [
   { featureType: 'water',         elementType: 'labels.text.fill',stylers: [{ color: '#7E9B9E' }] },
   { featureType: 'transit',       elementType: 'labels.icon',     stylers: [{ visibility: 'off' }] },
   { featureType: 'administrative',elementType: 'geometry.stroke', stylers: [{ color: '#C9B99A' }] },
+  { featureType: 'road.highway',  elementType: 'labels',          stylers: [{ visibility: 'off' }] },
+  { featureType: 'road.arterial', elementType: 'labels.icon',     stylers: [{ visibility: 'off' }] },
 ]
 
 const OURO_PRETO_CENTER = { lat: -20.3856, lng: -43.5035 }
