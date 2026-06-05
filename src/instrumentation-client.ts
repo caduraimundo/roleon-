a3047cbcd744390618aa4199b76a98818301499b
Carlos Raimundo <caduoliveiraraimundo@gmail.com>
2026-06-05 00:03:39 -0300

    fix: export onRouterTransitionStart for Sentry navigation instrumentation

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,4 +1,7 @@
+import * as Sentry from '@sentry/nextjs'
 import posthog from 'posthog-js'
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
