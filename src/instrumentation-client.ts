1c3d544e1203760d34e4aefa58d6361de02974f7
Carlos Raimundo <caduoliveiraraimundo@gmail.com>
2026-06-30 23:13:03 -0300

    fix: adicionar Sentry.init no instrumentation-client dentro de src/

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,6 +1,12 @@
 import * as Sentry from '@sentry/nextjs'
 import posthog from 'posthog-js'
 
+Sentry.init({
+  dsn: "https://ab0f83f81c6a8fa95892e55412a0aa66@o4511424620986368.ingest.us.sentry.io/4511424623673344",
+  tracesSampleRate: 1.0,
+  debug: false,
+})
+
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
