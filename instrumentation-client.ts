7b8517c265682b37541371d8b89f88b47d911ecb
Carlos Raimundo <caduoliveiraraimundo@gmail.com>
2026-07-01 09:22:33 -0300

    test: remover instrumentation-client.ts da raiz para confirmar se src/ e o arquivo ativo

--- instrumentation-client.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: "https://ab0f83f81c6a8fa95892e55412a0aa66@o4511424620986368.ingest.us.sentry.io/4511424623673344",
-  tracesSampleRate: 1.0,
-  debug: false,
-});
