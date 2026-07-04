a7d94981c85c21261cf57b403f19213d5cded683
Carlos Raimundo <caduoliveiraraimundo@gmail.com>
2026-07-04 00:21:14 -0300

    fix: exibir ProducerHeader apenas na tela de perfil do produtor

--- src/app/produtor/PortalHeader.tsx
+++ src/app/produtor/PortalHeader.tsx
@@ -73,7 +73,7 @@ export default function PortalHeader() {
           lineHeight: 1,
         }}>Produtor</span>
       </div>
-      <ProducerHeader />
+      {pathname === '/produtor/perfil' && <ProducerHeader />}
     </header>
   )
 }
